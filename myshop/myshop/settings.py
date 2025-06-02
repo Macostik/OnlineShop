33011420955b52e78e97f468f6e8fe5a8d5da092
Macostik <y.granchenko@gmail.com>
2025-06-02 08:12:33 +0300

    feat: oreder handling

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'orders.apps.OrdersConfig',
     'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
 ]
@@ -64,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                 'cart.context_processors.cart',
+                'cart.context_processors.cart',
             ],
         },
     },
--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static  # For serving media files during dev
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
